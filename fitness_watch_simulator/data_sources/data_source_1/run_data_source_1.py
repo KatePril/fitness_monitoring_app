5569f91557da4c2dc1739ed580dc0b11511f9aae
Kateryna Prylutska <prylutska.kateryna@lll.kpi.ua>
2025-05-03 12:18:45 +0300

    Add data generators

---
+++ fitness_watch_simulator/data_sources/data_source_1/run_data_source_1.py
@@ -0,0 +1,7 @@
+import time
+from fitness_watch_simulator.data_generation.data_generator import DataGenerator
+
+if __name__ == '__main__':
+    while True:
+        print(DataGenerator.generate_fitness_data(1))
+        time.sleep(1)
