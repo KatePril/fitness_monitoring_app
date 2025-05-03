03d8319ffc7e7e43d73be217afdbe152658ecac0
Kateryna Prylutska <prylutska.kateryna@lll.kpi.ua>
2025-05-03 17:55:57 +0300

    Add data generator test

---
+++ tests/test_data_generator.py
@@ -0,0 +1,19 @@
+import unittest
+
+from data_sources.data_generation.data_generator import DataGenerator
+
+
+class TestDataGeneration(unittest.TestCase):
+    def test_something(self):
+        user_id = 1
+        data = DataGenerator().generate_fitness_data(user_id)
+        self.assertIsNotNone(data.get("timestamp"))
+        self.assertIsNotNone(data.get("heart_rate"))
+        self.assertIsNotNone(data.get("saturation"))
+        self.assertIsNotNone(data.get("steps"))
+        self.assertEqual(user_id, data.get("user_id"))
+        self.assertEqual(5, len(data.keys()))
+
+
+if __name__ == '__main__':
+    unittest.main()
