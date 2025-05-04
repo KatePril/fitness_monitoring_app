fc8a2a5757cc53dda2c26e5e09a4e11df7f6bebe
Kateryna Prylutska <prylutska.kateryna@lll.kpi.ua>
2025-05-04 20:22:57 +0300

    Add Dockerfile to fitness_monitor_web_app. Update docker-compose.yaml

--- create_db.sql
+++ create_db.sql
@@ -7,12 +7,6 @@ CREATE TABLE IF NOT EXISTS app_user (
     height NUMERIC
 );
 
-INSERT INTO app_user(username, email, password, weight, height)
-VALUES
-    ('johndoe', 'johndoe@example.com', 'hashed_password_123', 75.5, 180),
-    ('janedoe', 'janedoe@example.com', 'hashed_password_456', 62.0, 165),
-    ('froglady', 'froglady@example.com', 'hashed_password_789', 68.2, 172);
-
 CREATE TABLE IF NOT EXISTS health_data (
     id SERIAL PRIMARY KEY,
     timestamp TIMESTAMP NOT NULL,
