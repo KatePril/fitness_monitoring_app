37a7f8f9cf0144b0750e47eace6ed6df402efabb
Kateryna Prylutska <prylutska.kateryna@lll.kpi.ua>
2025-05-04 16:37:51 +0300

    Add user insertion

---
+++ fitness_monitor_web_app/src/registration_login/create_user.py
@@ -0,0 +1,27 @@
+from fitness_monitor_web_app.src.entities.user import User
+
+INSERTION_QUERY = """
+    INSERT INTO app_user(username, email, password, weight, height)
+    VALUES (%s, %s, %s, %s, %s)
+    RETURNING user_id
+"""
+
+def create_user(user: User, cursor, conn) -> int|None:
+    try:
+        cursor.execute(
+            INSERTION_QUERY,
+            (
+                user.username,
+                user.email,
+                user.password,
+                user.weight,
+                user.height
+            )
+        )
+        user_id = cursor.fetchone()[0]
+        conn.commit()
+        return user_id
+    except Exception as e:
+        conn.rollback()
+        print("Error:", e)
+        return None
