e453e1ae691d509bf781e56e205ac9cf0cd2b5d3
Kateryna Prylutska <prylutska.kateryna@lll.kpi.ua>
2025-05-04 16:37:17 +0300

    Add password hashing

---
+++ fitness_monitor_web_app/src/entities/user.py
@@ -0,0 +1,8 @@
+class User:
+    def __init__(self, user_id, username, email, password, weight, height):
+        self.user_id = user_id
+        self.username = username
+        self.email = email
+        self.password = password
+        self.weight = weight
+        self.height = height
