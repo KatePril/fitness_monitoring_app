14f3487c8e492fac5bc2f293b48a748bab8cc5df
Kateryna Prylutska <prylutska.kateryna@lll.kpi.ua>
2025-05-07 23:49:00 +0300

    Delete print

--- fitness_monitor_web_app/src/registration_login_edit/password.py
+++ fitness_monitor_web_app/src/registration_login_edit/password.py
@@ -4,5 +4,4 @@ def get_password_hash(password):
     return generate_password_hash(password)
 
 def check_password(password, password_hash):
-    print(generate_password_hash(password))
     return check_password_hash(password_hash, password)
